f9fecf434eb0c2528de5209236dc3691861ee032
Atif Anwar <125822126+Matif408@users.noreply.github.com>
2023-11-10 21:33:00 +0500

    problem solve

---
+++ practice/index.js
@@ -0,0 +1,37 @@
+"use strict";
+// Even or Odd
+
+/*
+const isEvenOdd = function (number) {
+  return number % 2 === 0 ? "Number is even" : "Number is odd";
+};
+
+console.log(isEvenOdd(8));
+*/
+
+//Write a function that accepts two parameters (sum and multiply) and find two numbers [x, y], where x + y = sum and x * y = multiply.
+
+/*
+let sum = 0;
+
+let multiply = 0;
+const twoNumber = [];
+const valueSumAndMultiply = function (number1, number2) {
+  sum = number1 + number2;
+  multiply = number1 * number2;
+  twoNumber.push(number1, number2);
+  twoNumber.sort();
+};
+valueSumAndMultiply(8, 6);
+console.log(sum, multiply, twoNumber);
+*/
+
+// factorial of number
+/*
+let factorialOfNumber = 5;
+let fact = 1;
+for (let counter = 1; counter <= factorialOfNumber; counter++) {
+  fact = counter * fact;
+}
+console.log(fact);
+*/
