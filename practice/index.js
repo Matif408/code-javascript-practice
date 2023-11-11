30569921888c0b0916c080dceee7db2d9ff1d14e
Atif Anwar <125822126+Matif408@users.noreply.github.com>
2023-11-11 22:43:08 +0500

    new program

--- practice/index.js
+++ practice/index.js
@@ -35,3 +35,50 @@ for (let counter = 1; counter <= factorialOfNumber; counter++) {
 }
 console.log(fact);
 */
+
+//You get an array of numbers, return the sum of all of the positives ones. Example [1,-4,7,12] => 1 + 7 + 12 = 20
+/*
+let sum = 0;
+
+const sumOfNumber = function (array) {
+  for (let arrayOfIndex = 0; arrayOfIndex < array.length; arrayOfIndex++) {
+    if (Math.sign(array[arrayOfIndex]) !== -1) {
+      sum = array[arrayOfIndex] + sum;
+    } else if (array[arrayOfIndex === -1]) {
+      return sum;
+    }
+  }
+  return sum;
+};
+console.log(sumOfNumber([1, 789, 2]));
+*/
+
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+/*
+const makeNegative = function (number) {
+  return -Math.abs(number);
+};
+console.log(makeNegative(-7));
+*/
+// Complete the solution so that it reverses the string passed into it.
+
+/*
+const reverseString = function (word) {
+  return word.split("").reverse("").join("");
+};
+console.log(reverseString("hello"));
+*/
+// Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7.
+/*
+const areaOfTriangle = function (s1, s2, s3) {
+  let semiParametar = (s1 + s2 + s3) / 2;
+
+  return Math.sqrt(
+    semiParametar *
+      (semiParametar - s1) *
+      (semiParametar - s2) *
+      (semiParametar - s3)
+  );
+};
+console.log(areaOfTriangle(5, 6, 7));
+*/
